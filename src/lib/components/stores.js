2a2a57e4c1848fb4459f916e19c77f8269d437f1
Sean Shih-Lun Wu <slseanwu@Seans-MacBook-Pro-5.local>
2025-04-08 20:39:45 -0400

    fix tooltip linechart and layer change

--- src/lib/components/stores.js
+++ src/lib/components/stores.js
@@ -1,6 +1,8 @@
 // stores.js
 import { writable } from 'svelte/store';
 
+export const layerDataStore = writable(null);
+
 export const hoveredDataStore = writable(null);
 
 export const tooltipPositionStore = writable({ x: 0, y: 0 });
